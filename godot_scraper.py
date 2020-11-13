bd52e9e5f8b5e4a5dc753d372d2e67c64a062171
AndreaTerenziani <andrea.terenziani.at@gmail.com>
2020-11-13 01:00:56 +0100

    misc fixes

--- godot_scraper.py
+++ godot_scraper.py
@@ -22,26 +22,22 @@ def get_page_links(url):
 
     return output
 
-def get_filtered_urls(url, regex):
+def get_filtered_links(url, regex):
     return list(filter(lambda u : re.search(regex, u), get_page_links(url)))
 
-def print_array(arr):
-    for a in arr:
-        print(a)
-
-def get_output_dir():
-    if (d := input("Output location (Enter to use current folder) >> ")) == "":
-        d = str(Path.cwd())
-    elif not(Path(d).is_absolute()):
-        home = str(Path.home())
-        home_as_root = input(ct(f"Path isn't absolute - use home directory {home} as path root [Y/n]? ", "yellow"))
-
-        if (home_as_root != "n"):
-            d = home + "/" + d  
-        
-    return d
-
 if __name__ == '__main__':
+
+    def get_output_dir():
+        if (d := input("Output location (Enter to use current folder) >> ")) == "":
+            d = str(Path.cwd())
+        elif not(Path(d).is_absolute()):
+            home = str(Path.home())
+            home_as_root = input(ct(f"Path isn't absolute - use home directory {home} as path root [Y/n]? ", "yellow"))
+
+            if (home_as_root != "n"):
+                d = home + "/" + d  
+            
+        return d
 
     version_file = ".version"
     last_download = ""
@@ -54,7 +50,7 @@ if __name__ == '__main__':
 
     #PHASE 1: Get the link to the last version's directory
 
-    urls = get_filtered_urls("https://downloads.tuxfamily.org/godotengine/", r".*/(\d\.*)+$")
+    urls = get_filtered_links("https://downloads.tuxfamily.org/godotengine/", r".*/(\d\.*)+$")
     last_stable_url = urls[-2]+"/"
     last_url = urls[-1]+"/"
 
@@ -63,15 +59,15 @@ if __name__ == '__main__':
 
     #PHASE 2: Check if the last version has a stable release (aka if its page contains a link to a zip file)
 
-    all_urls = get_filtered_urls(last_url, "")
-    urls = get_filtered_urls(last_url, archive_regex := r"x11\.64\.zip$")
+    all_urls = get_filtered_links(last_url, "")
+    urls = get_filtered_links(last_url, archive_regex := r"x11\.64\.zip$")
 
     if len(urls) == 0:
         proceed = input(ct(f"Latest version ({last}) doesn't have a stable release yet - download anyway [Y/n]? ", "yellow")).lower()
 
         if (proceed in ["", "y"]):        
             #PASE 2b: if the latest version is not stable yet, get the link to the latest beta/rc
-            urls = get_filtered_urls(all_urls[-1]+"/", archive_regex)
+            urls = get_filtered_links(all_urls[-1]+"/", archive_regex)
             print(ct(f"Selected version: {last}", "green"))
         else:
             proceed = input(ct(f"Download latest stable version ({last_stable}) instead [Y/n]? ", "yellow")).lower()
@@ -79,7 +75,7 @@ if __name__ == '__main__':
             if (proceed == "n"):
                 exit(0)
             
-            urls = get_filtered_urls(last_stable_url, archive_regex)
+            urls = get_filtered_links(last_stable_url, archive_regex)
             print(ct(f"Selected version: {last_stable}", "green"))
     
     #PHASE 3: Download the zip file
